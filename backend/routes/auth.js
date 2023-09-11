0c6bc0fb27f068ec584c73735c6e7e1fe136fe31
Akash <kumawat.aakash515@gmail.com>
2023-09-11 17:51:24 +0530

    creates login end point and sending authtokens

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -61,7 +61,44 @@ const authtoken = jwt.sign(data, JWT_SECRET);
 res.json(authtoken)
 }catch (error){
     console.error(error.message);
-    res.status(500).send("some error occured");
+    res.status(500).send("internal error occured");
 }
 });
+// authenticate a user using post
+router.post("/login",[
+    body('email', 'enter a vaild email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+],
+     async(req, res) => {{
+//if there are errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) 
+      return res.status(400).json({errors: errors.array()});
+    }
+    const {email, password}=req.body;
+    try {
+        let user = await User.findone({email,password});
+        if(user){
+            return res.status(400).json({error:"please use correct credentials"});
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"please try to login with correct credentials"});
+
+        }
+
+        const data ={
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken =jwt.sign(data,JWT_SECRET);
+        res.json(authtoken)
+
+    } catch (error){
+        console.error(error.message);
+        res.status(500).send("internal error occured");
+    }
+
+})
 module.exports = router;
