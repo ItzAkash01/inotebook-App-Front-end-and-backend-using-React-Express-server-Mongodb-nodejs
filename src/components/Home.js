aba8b4195ae8137a7d55dd86191585bfa13b9fac
Akash <kumawat.aakash515@gmail.com>
2023-09-13 16:49:29 +0530

    frontend Setup done

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import {
+    BrowserRouter as router,
+    Switch,
+    Route,
+    Link,
+  } from "react-router-dom";
+  
+  export const Home = () =>{
+    return(
+        <div>
+            This is home
+        </div>
+    )
+  }
