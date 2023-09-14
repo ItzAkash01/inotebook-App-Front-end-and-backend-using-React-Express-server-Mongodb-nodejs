9295c3769c56c34a2518e9e91e8ec815451c6971
Akash <kumawat.aakash515@gmail.com>
2023-09-14 16:51:46 +0530

    fetching notes from context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
-
-  import React from 'react'
-  
-  const Home = () => {
+import React,{useContext} from 'react'
+import noteContext from "../context/noteContext";
+  export const Home = () => {
+    const context = useContext(noteContext);
+  const{notes,setNotes}=context;
     return (
       <div>
       <div className="container my-3">
@@ -25,6 +26,9 @@
 </form>
 
        <h2>Your Notes</h2>
+       {notes.map((note)=>{
+        return note.title;
+       })}
        </div> 
       </div>
     )
--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -2,6 +2,35 @@
 import noteContext from "./noteContext";
 import { useState } from "react";
 const NoteState =(props)=>{
+
+const notesInitial = [
+{
+
+"_id":"61322f19553781a8cad0e065",
+"user":"6131dc5e3e40637dc4734a068",
+"title":"My title",
+"description":"please wake up early",
+"tag":"personal",
+},
+{
+
+    "_id":"61322f19553781a8cad0e065",
+    "user":"6131dc5e3e40637dc4734a068",
+    "title":"My title",
+    "description":"please wake up early",
+    "tag":"personal",
+    }
+
+]
+
+
+
+
+
+
+
+
+
 //     const S1 = {
 //         "name":  "aakash" ,
 //         "class": "5b"
@@ -16,8 +45,11 @@ const NoteState =(props)=>{
             
 //         },1000); //{state, update}
 //     }
+
+
+const [notes, setNotes] = useState(notesInitial);
 return(
-<noteContext.Provider value={{}}>
+<noteContext.Provider value={{notes}}>
     {props.children}
 </noteContext.Provider>
 
