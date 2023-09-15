2c1ff129d5bdc0004fa5cfc3f78559f6703957f6
Akash <kumawat.aakash515@gmail.com>
2023-09-15 15:13:24 +0530

    font icons added

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import {
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/NoteState";
-
+import Alert from "./components/Alert";
 function App() {
   return (
     <>
     <NoteState>
       <Router>
         <Navbar />
+        <Alert message ="This is an amazing React course"/>
         <div className="container">
         <Routes>
           <Route path="*" element= {<Home />}></Route>
--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -6,20 +6,74 @@ const NoteState =(props)=>{
 const notesInitial = [
 {
 
-"_id":"61322f19553781a8cad0e065",
-"user":"6131dc5e3e40637dc4734a068",
+"_id":"61322f195533781a8cad0e0651",
+"user":"6131dc5e3e40637dcjh4734a068",
 "title":"My title",
 "description":"please wake up early",
 "tag":"personal",
 },
 {
 
-    "_id":"61322f19553781a8cad0e065",
-    "user":"6131dc5e3e40637dc4734a068",
+    "_id":"613322f19553781a8cad0e0652",
+    "user":"6131dc5e3e406sd37dc4734a068",
+    "title":"My title",
+    "description":"please wake up early",
+    "tag":"personal",
+    },
+    {
+
+        "_id":"613322f19553781a8cad0e0653",
+        "user":"6131dc5e3hge40637dc4734a068",
+        "title":"My title",
+        "description":"please wake up early",
+        "tag":"personal",
+        }
+,
+{
+
+    "_id":"613223f19553781a8cad0e0654",
+    "user":"6131dfdc5e3e40637dc4734a068",
     "title":"My title",
     "description":"please wake up early",
     "tag":"personal",
     }
+,
+{
+
+    "_id":"611322f19553781a8cad0e0655",
+    "user":"6131dc5e3gfe40637dc4734a068",
+    "title":"My title",
+    "description":"please wake up early",
+    "tag":"personal",
+    },
+    {
+
+        "_id":"61322f139553781a8cad0e0656",
+        "user":"6131dc5safe3e40637dc4734a068",
+        "title":"My title",
+        "description":"please wake up early",
+        "tag":"personal",
+        }
+,
+{
+
+    "_id":"61322f195543781a8cad0e0657",
+    "user":"6131dc5e3eaw40637dc4ds734a068",
+    "title":"My title",
+    "description":"please wake up early",
+    "tag":"personal",
+    },
+    {
+
+        "_id":"61322f193553781a8cad0e0658",
+        "user":"6131dc5e3e40637dc4d7ds34a068",
+        "title":"My title",
+        "description":"please wake up early",
+        "tag":"personal",
+        }
+    
+
+
 
 ]
 
